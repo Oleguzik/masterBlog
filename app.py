4eaec7c33e6017d185182e43fa000af15153309b
oleguzik <guzik.oleg@gmail.com>
2025-11-20 21:24:34 +0100

    feature/ fetching and render json data

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask import render_template
+import json
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Load articles from JSON file
+    with open('articles.json', 'r') as f:
+        posts = json.load(f)
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5015, debug=True)

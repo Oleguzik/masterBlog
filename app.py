5560b548403407feec8b53e358c9a31c1e49d70e
oleguzik <guzik.oleg@gmail.com>
2025-11-21 09:34:20 +0100

    feature/ add new post

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -10,6 +9,45 @@ def index():
     with open('articles.json', 'r') as f:
         posts = json.load(f)
     return render_template('index.html', posts=posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # Load existing posts
+        with open('articles.json', 'r') as f:
+            posts = json.load(f)
+        
+        # Generate new ID
+        if posts:
+            new_id = max(post['id'] for post in posts) + 1
+        else:
+            new_id = 1
+        
+        # Get form data
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        
+        # Create new post
+        new_post = {
+            'id': new_id,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+        
+        # Append to posts
+        posts.append(new_post)
+        
+        # Save back to JSON
+        with open('articles.json', 'w') as f:
+            json.dump(posts, f, indent=4)
+        
+        # Redirect to home
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
 
 
 if __name__ == '__main__':
